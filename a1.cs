24c43b4a46ea0aa439ffe5b4208a1bd900f66644
MeirSadon <45739995+MeirSadon@users.noreply.github.com>
2019-01-09 17:49:32 +0200

    Add files via upload

---
+++ a1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a number:");
+			int x = Convert.ToInt32(Console.ReadLine());
+			Power2(x);
+			Convert.ToInt32(Console.ReadLine());
+		}
+		
+
+		private static void Power2(int i)
+		{
+			i = i * 2;
+			Console.WriteLine($"multiply 2 of your number is {i}");
+		}
+
+	}
+}
